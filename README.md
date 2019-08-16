9ddc10f4a3b8bddc5557e1743e1da09d411d44e1
Keith Chambers <kd.chambers97@gmail.com>
2019-08-16 01:48:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Dear ImGui Project starter 
+
+The purpose of this project is to just get up and started with a new Dear Imgui project with minimal setup
+
+## Requirements
+
+The basic requirements are as follows
+
+Linux
+CMake 3.8 +
+make
+C++17 capable Compiler (E.G gcc or clang)
+OpenGL 
+X11
+
+There is probably many more dependencies, but those are the high-level ones. 
+
+## Usage
+
+	cmake .
+	make 
+	
+	./exampleproject
+	
+Once you have ensured that works, go into CMakeLists.txt and change it to reflect your project
+
+
+	
---
+++ src/main.cpp
@@ -0,0 +1,94 @@
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+#include <GL/gl3w.h>
+#include <GLFW/glfw3.h>
+
+#include <stdio.h>
+#include <string.h>
+
+static void glfw_error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+}
+
+int main()
+{
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit())
+        return 1;
+
+    const char* glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // Make Window Unresizeable
+
+    // Create window with graphics context
+    GLFWwindow* window = glfwCreateWindow(1280, 720, " ImGui IDE", nullptr, nullptr);
+    if (window == nullptr)
+        return 1;
+    
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+
+    if ( gl3wInit() != 0 )
+    {
+        fprintf(stderr, "Failed to initialize OpenGL loader!\n");
+        return 1;
+    }
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+    ImGui::StyleColorsDark();
+
+    // Setup Platform/Renderer bindings
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        
+        /*
+         
+         PUT CODE HERE
+         
+         */
+
+        // Rendering
+        ImGui::Render();
+        int display_w, display_h;
+        glfwMakeContextCurrent(window);
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwMakeContextCurrent(window);
+        glfwSwapBuffers(window);
+    }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
